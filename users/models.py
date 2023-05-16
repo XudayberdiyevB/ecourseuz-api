65f0003547c16ab6b2b599c6863e10bf4836829b
bunyod_backend_developer <122611882+BunyodNaimov@users.noreply.github.com>
2023-05-16 11:10:14 +0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -12,10 +12,11 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, unique=True)
     job = models.CharField(max_length=129)
-    profile_picture = models.ImageField(upload_to='profile_picture/', null=True,
-                                        default='../media/profile_picture/default.png')
+    profile_picture = models.ImageField(
+        upload_to='profile_picture/', null=True, default='../media/profile_picture/default.png'
+    )
     address = models.CharField(max_length=256)
-    birth_date = models.DateField(null=False)
+    birth_date = models.DateField()
     age = models.IntegerField(null=True)
     objects = CustomUserManager()
     USERNAME_FIELD = "email"
