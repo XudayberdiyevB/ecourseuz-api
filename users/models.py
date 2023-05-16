829c1c5253e44121b66c319e4dc03933d5969dad
hacker <naimov.bunyod2021@gmail.com>
2023-05-16 14:33:30 +0500

    phone --> null=True job --> null=True

--- users/models.py
+++ users/models.py
@@ -13,8 +13,8 @@ class User(AbstractUser):
 
     username = models.CharField(max_length=32, unique=True, null=True)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15, unique=True)
-    job = models.CharField(max_length=129)
+    phone = models.CharField(max_length=15, unique=True, null=True)
+    job = models.CharField(max_length=129, null=True)
     profile_picture = models.ImageField(upload_to='profile_picture/', null=True)
     address = models.CharField(max_length=256, null=True)
     birth_date = models.DateField(null=True)
