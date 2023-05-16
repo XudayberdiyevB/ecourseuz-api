6c2ae93b20ccbe3cdf567374872dd983640c6fa4
Tukhtasinov-Saydullo <tukhtasinov.saydullo@yahoo.com>
2023-05-16 14:23:47 +0500

    Fixed problems: models.py

--- users/models.py
+++ users/models.py
@@ -11,10 +11,10 @@ class User(AbstractUser):
     username = models.CharField(max_length=32, unique=True, null=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, unique=True)
-    job = models.CharField(max_length=129)
+    job = models.CharField(max_length=129, null=True)
     profile_picture = models.ImageField(upload_to='profile_picture/', null=True)
-    address = models.CharField(max_length=256)
-    birth_date = models.DateField()
+    address = models.CharField(max_length=256, null=True)
+    birth_date = models.DateField(null=True)
     age = models.IntegerField(null=True)
     objects = CustomUserManager()
     USERNAME_FIELD = "email"
