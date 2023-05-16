feee332cbe26f273ddbc10b5753a38fdb1758af8
bunyod_backend_developer <122611882+BunyodNaimov@users.noreply.github.com>
2023-05-16 11:35:35 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'users.User'
