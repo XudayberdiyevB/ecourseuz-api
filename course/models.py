26a97ee8a2cf7e182887e8ce43c710c1877945dc
davlat <davlatbekm004@gmail.com>
2023-05-16 19:34:24 +0500

    add course model

--- course/models.py
+++ course/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField()
+    desc = models.TextField()
+    price = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField()
+    level = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_course'
+    )
+    category = models.ForeignKey(
+        None,
+        on_delete=models.CASCADE,
+        related_name='category_course'
+    )
+    image = models.ImageField(upload_to='/course_picture')
+    video = models.FileField(upload_to='/course_video')
+    created_add = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
