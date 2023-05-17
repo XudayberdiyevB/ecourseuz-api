e7500ee56c3bc2a2939f995954a42c2ec8ad10fe
alfa77777 <boburtohirov66@gmail.com>
2023-05-17 14:46:50 +0500

    CourseApply and Review

--- course/models.py
+++ course/models.py
@@ -48,3 +48,53 @@ class CourseContent(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ApplyStatus(models.Choices):
+    UNPAID = 'Unpaid', "unpaid"
+    PAID = 'Paid', "paid"
+
+
+class Rate(models.Choices):
+    CHOICE_ONE = 1
+    CHOICE_TWO = 2
+    CHOICE_THREE = 3
+    CHOICE_FOUR = 4
+    CHOICE_FIVE = 5
+
+
+class CourseApply(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="course"
+    )
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user"
+    )
+    status = models.CharField(max_length=20, choices=ApplyStatus.choices)
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user"
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="course"
+    )
+    rate = models.PositiveIntegerField(max_length=20, choices=Rate.choices)
+    comment = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user)
