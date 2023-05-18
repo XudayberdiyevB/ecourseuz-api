550bb65374c20ac6d2f204468630ef64e9b7e227
islomov_ibrohimjon <isloomoov@gmail.com>
2023-05-18 19:13:59 +0500

    VIEW has been created

--- course/urls.py
+++ course/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from course.views import (
     CourseApplyView,
-    CourseApplyDetailView
+    CourseApplyDetailView,
+    CourseContentApiView,
+    CourseContentDetilView
 )
 
 urlpatterns = [
     path("course-apply/", CourseApplyView.as_view(), name="course_apply"),
     path("course-apply/<int:pk>/", CourseApplyDetailView.as_view(), name="course_apply_detail"),
+    path("course-content/", CourseContentApiView.as_view(), name="course_content"),
+    path("course-content/<int:pk>/", CourseContentDetilView.as_view(), name="course_conten-detil"),
 ]
--- course/views.py
+++ course/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
-from course.models import CourseApply
-from course.serializers import CourseApplySerializer
+from course.models import CourseApply, CourseContent
+from course.serializers import CourseApplySerializer, CourseContentSerializer
 
 
 class CourseApplyView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def get(self, request, *args, **kwargs):
         course_applies = CourseApply.objects.all()
         serializer = CourseApplySerializer(course_applies, many=True)
 
         return Response(serializer.data)
-    
+
     def post(self, request, *args, **kwargs):
         serializer = CourseApplySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -31,7 +32,7 @@ class CourseApplyDetailView(CourseApplyView):
         serializer = CourseApplySerializer(course_apply)
 
         return Response(serializer.data)
-    
+
     def put(self, request, *args, **kwargs):
         course_apply = get_object_or_404(CourseApply, id=kwargs.get('pk'))
         serializer = CourseApplySerializer(instance=course_apply, data=request.data)
@@ -39,3 +40,17 @@ class CourseApplyDetailView(CourseApplyView):
         serializer.save()
 
         return Response(serializer.data)
+
+
+class CourseContentApiView(generics.ListCreateAPIView):
+    queryset = CourseContent.objects.all()
+    serializer_class = CourseContentSerializer
+
+
+class CourseContentDetilView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CourseContent.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CourseContentSerializer
+        return CourseContentSerializer
