7e93385c6c14886ae1efb0f994e4dc9be6884d99
oktamov <122670933+oktamov@users.noreply.github.com>
2023-05-18 23:10:54 +0500

    category ochirildi

--- common/serializers.py
+++ common/serializers.py
@@ -6,7 +6,7 @@ from common.models import ApplicationForm, Category, ContactUs, ContactForm
 class ApplicationFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplicationForm
-        fields = ['course', 'name', 'email', 'category']
+        fields = ['course', 'name', 'email']
 
 
 class CategoryListSerializers(serializers.ModelSerializer):
