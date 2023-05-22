53cddf8bb6cf62fd79ee653ca2ab80ffac37eebc
Yunusboyev Farruxbek <farruh@yunusboyev.com>
2023-05-22 11:20:54 +0500

    test

--- course/tests.py
+++ course/tests.py
@@ -8,13 +8,58 @@ client = Client()
 
 
 class TestCourseApply(TestCase):
+    def __init__(self, methodName: str = ...):
+        super().__init__(methodName)
+        self.new_course = None
+
     def setUp(self) -> None:
-        self.course = Course.objects.create(name="Courses")
-        self.user = User.objects.create(username="Farruxbek", email="yunusboyevfarruxbek@gmail.com")
-        new_course = {
-            "status": "new stats",
+        self.course = Course.objects.create()
+        self.user = User.objects.create()
+        self.course_apply = CourseApply.objects.create(
+            status="New status",
+            course=self.course,
+            user=self.user
+
+        )
+
+    new_course = {
+        "status": "new stats",
+        "course": "new course",
+        "user": "new users"
+    }
+
+    def test_course_apply_create(self):
+        url = reverse("course_apply")
+
+        response = client.post(url, data=self.new_course)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertNotEqual(response.status_code, 400)
+        self.assertEqual(response.data["status"], self.new_course["status"])
+
+    def test_course_apply_detail(self):
+        url = reverse("course_apply_detail", kwargs={"slug": self.course_apply.slug})
+
+        response = client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.course_apply.title, "New product1")
+
+    def test_product_update(self):
+        url = reverse("course_apply_detail", kwargs={"slug": self.course_apply.slug})
+        data = {
+            "status": "string",
             "course": self.course.id,
-            "users": self.user.id
+            "user": self.user.id
         }
+        response = client.put(url, data=data, content_type="application/json")
 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["title"], data["title"])
 
+    def test_product_delete(self):
+        url = reverse("product_detail", kwargs={"slug": self.course_apply.slug})
+
+        response = client.delete(url)
+
+        self.assertEqual(response.status_code, 204)
